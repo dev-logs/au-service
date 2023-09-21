17050ff545aa2e70f9e10ec6a1e8ce619bcf83ac
tiendang <tiendvlp@gmail.com>
2023-09-21 23:59:15 +0700

    Setup logger

--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,15 @@ mod services;
 use axum::{Router, Server};
 use std::net::{IpAddr, Ipv4Addr, SocketAddr};
 
-use env_logger;
+use pretty_env_logger::formatted_timed_builder;
 use crate::api::controllers::user::UserController;
 
 #[tokio::main]
 async fn main() {
-    let mut log_builder = pretty
+    let mut log_builder = formatted_timed_builder();
+    log_builder.filter(None, log::LevelFilter::Info);
+    log_builder.init();
+
     let main_route = Router::new().merge(UserController::new().router);
     Server::bind(&SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 3000))
         .serve(main_route.into_make_service())
