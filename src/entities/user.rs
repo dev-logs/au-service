88af793a2605cbfdea1cd8172c9525c520f771c5
tiendang <tiendvlp@gmail.com>
2023-09-23 21:48:14 +0700

    Add id field to entity

--- src/entities/user.rs
+++ src/entities/user.rs
@@ -1,7 +1,9 @@
 use serde::{Deserialize, Serialize};
+use surrealdb::sql::Thing;
 
 #[derive(Debug, Deserialize, Serialize, Clone)]
 pub struct User {
+    pub id: Option<Thing>,
     pub name: String,
     pub password: String,
     pub email: String,
