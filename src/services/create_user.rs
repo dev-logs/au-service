708752e7030d6f91ba5fb937eb551ce9b83a84a7
tiendvlp <tiendvlp@gmail.com>
2023-10-17 00:25:00 +0700

    Fix bug for surreal derive

--- src/services/create_user.rs
+++ src/services/create_user.rs
@@ -3,7 +3,10 @@ use crate::core_utils::errors::OurErrors;
 use crate::entities::user::User;
 use crate::services::base::{OurService, OurResult};
 use async_trait::async_trait;
+use log::info;
+use serde::{Deserialize, Serialize};
 use surreal_derive::surreal_quote;
+use tokio::io::AsyncReadExt;
 
 #[derive(Clone)]
 pub struct CreateUserService {
@@ -15,9 +18,23 @@ pub struct Params {
     pub user: User
 }
 
+#[derive(Debug, Serialize, Deserialize, Clone)]
+struct Name<'a> {
+    first: &'a str,
+    last: &'a str,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+struct Person<'a> {
+    title: &'a str,
+    name: Name<'a>,
+    marketing: bool,
+}
+
 #[async_trait]
 impl OurService<Params, User> for CreateUserService {
     async fn execute(self, params: Params) -> OurResult<User> {
+        info!("created user");
         if let Some(created_user) = self.db.query(surreal_quote!("CREATE #record(&params.user)")).await?.take(0)? {
            return Ok(created_user);
         }
